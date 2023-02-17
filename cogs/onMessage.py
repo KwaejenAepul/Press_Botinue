e7655559b6c9c12b050cf6f99cb79916b356d16e
KwaejenAepul <66973949+KwaejenAepul@users.noreply.github.com>
2023-02-17 20:36:54 +0100

    word filter update
    now reads a file with all the words that should be filtered out at start up.
    when adding a word it appends it at the end of the file with filtered words

--- cogs/onMessage.py
+++ cogs/onMessage.py
@@ -11,22 +11,29 @@ class onMessage(commands.Cog):
     #Word filter related stuff
     @commands.Cog.listener()
     async def on_ready(self):
-        pass
+        with open("naughtywords.txt", "r") as f:
+            lines = f.readlines()
+            for i in lines:   
+                self.bad_words.append(i.strip())
+        f.close()
+        print(self.bad_words)
 
     @commands.command()
     @commands.has_permissions(ban_members = True)
     async def add_word(self, ctx):
         contents = ctx.message.content.split()
         word = contents[1]
+        with open("naughtywords.txt", "a") as f:
+            f.write(f"\n{word}")
         self.bad_words.append(word)
-
+        print(self.bad_words)
 
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.author == self.bot.user:
             return
         if not message.content.startswith("!add_word"):
-            contents = message.content.split()
+            contents = message.content.lower().split()
             for word in self.bad_words:
                 if word in contents:
                     await message.delete()
